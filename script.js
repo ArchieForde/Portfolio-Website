3e28ac00892556ee4e32a0de81fa8392a531c87f
archie_forde18 <archieforde18@gmail.com>
2025-06-06 20:05:34 +0100

    Added a few bits

--- script.js
+++ script.js
@@ -9,28 +9,20 @@ typewriter
   .pauseFor(300)
   .typeString("Hey, I'm <span class='text-neon-cyan'>Archie</span><br>")
   .pauseFor(400)
-  .typeString("Software Engineer and <span class='text-neon-cyan'>AI Enthusiast.</span><br>")
+  .typeString("Software Engineer and <span class='text-neon-cyan'>AI Enthusiast.</span>")
   .pauseFor(200)
   .pauseFor(1000)
   .deleteAll()
   .start();
 
+const toggle = document.getElementById('menu-toggle');
+const menu = document.getElementById('mobile-menu');
 
-  const toggle = document.getElementById('menu-toggle');
-  const menu = document.getElementById('mobile-menu');
-
-  toggle.addEventListener('change', () => {
-    if (toggle.checked) {
-      menu.classList.remove('hidden');
-    } else {
-      menu.classList.add('hidden');
-    }
-  });
-  document.querySelectorAll('.menu-nav a').forEach(link => {
-    link.addEventListener('click', () => {
-      toggle.checked = false;
-      menu.classList.add('hidden');
-    });
-    });
-
-
+// Toggle menu visibility on checkbox change
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    menu.classList.remove('hidden'); // Show menu
+  } else {
+    menu.classList.add('hidden'); // Hide menu
+  }
+});
